ca4990871a70c7e7d28809c5fab55d4adf7bd05e
meetviradiya <80027043+meetviradiya@users.noreply.github.com>
2024-09-25 17:43:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// // JavaScript to handle the hamburger menu toggle
+// const hamburger = document.getElementById('hamburger');
+// const navbarLinks = document.getElementById('navbar-links');
+
+// hamburger.addEventListener('click', () => {
+//     // Toggle the 'active' class for the navbar links and hamburger menu
+//     navbarLinks.classList.toggle('active');
+//     hamburger.classList.toggle('active');
+// });
