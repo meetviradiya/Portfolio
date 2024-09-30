77dfb17e152cc6fa836df2a0f81ab86c0b9e8c15
meetviradiya <80027043+meetviradiya@users.noreply.github.com>
2024-09-30 17:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// // JavaScript to handle the hamburger menu toggle
-// const hamburger = document.getElementById('hamburger');
-// const navbarLinks = document.getElementById('navbar-links');
 
-// hamburger.addEventListener('click', () => {
-//     // Toggle the 'active' class for the navbar links and hamburger menu
-//     navbarLinks.classList.toggle('active');
-//     hamburger.classList.toggle('active');
-// });
