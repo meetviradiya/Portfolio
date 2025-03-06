f08688f32a337d82dccebd84846446c9c04c0282
meetviradiya <80027043+meetviradiya@users.noreply.github.com>
2025-03-06 10:45:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.querySelector('.navbar');
+    const masthead = document.querySelector('.masthead');
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+  
+    function adjustMastheadHeight() {
+      if (masthead && navbar) {
+        masthead.style.height = `calc(100vh - ${navbar.offsetHeight}px)`;
+      }
+    }
+  
+    // Initial adjustment and update on window resize
+    adjustMastheadHeight();
+    window.addEventListener('resize', adjustMastheadHeight);
+  
+    // Mobile menu toggle
+    if (menuIcon && navLinks) {
+      menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        adjustMastheadHeight();
+      });
+    }
+  });
+  
